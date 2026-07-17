8caab3e7e7e183cff14f4de2136a64eb24a11157
kapildev3006 <kdev7830@gmail.com>
2026-07-17 16:56:04 +0530

    fix: generate .env file during vercel build

--- gym_owner_web/build.sh
+++ gym_owner_web/build.sh
@@ -3,5 +3,7 @@ if [ ! -d "flutter" ]; then
   git clone https://github.com/flutter/flutter.git -b stable --depth 1
 fi
 ./flutter/bin/flutter config --enable-web
+echo "API_URL=$API_URL" > .env
+echo "SOCKET_URL=$SOCKET_URL" >> .env
 ./flutter/bin/flutter pub get
 ./flutter/bin/flutter build web --release
